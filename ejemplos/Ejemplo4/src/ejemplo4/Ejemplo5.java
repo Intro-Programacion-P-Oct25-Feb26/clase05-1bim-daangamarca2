b53f4a3b2c7c96e5cdf9c2c5fa14666960b23a8e
daangaamarca2 <arturoangamarca120@gmail.com>
2025-11-12 12:32:29 -0500

    negacion

--- ejemplos/Ejemplo4/src/ejemplo4/Ejemplo5.java
+++ ejemplos/Ejemplo4/src/ejemplo4/Ejemplo5.java
@@ -24,7 +24,7 @@ public class Ejemplo5 {
         System.out.println("Ingrese la ciudad");
         String ciudad = entrada.nextLine(); // Quito
 
-        if (!ciudad.equals("Loja")) {
+        if (!ciudad.equals("Loja")) {  //estoy negando la variable de tipo cadena
             System.out.printf("%s\n", "acceso correcto");
         } else {
             System.out.printf("%s\n", "acceso incorrecto");
@@ -32,7 +32,7 @@ public class Ejemplo5 {
 
         int numero = 10;
 
-        if (numero != 10) {
+        if (numero != 10) {  //estoy negando la variable de tipo numerica
             System.out.println("correcto");
         }
 
